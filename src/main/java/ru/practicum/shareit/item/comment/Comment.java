f3c18bac49f3025ca4f81cecd39aee860800be17
Aleksandr Nikolaev <i@an-98.ru>
2025-08-17 20:19:32 +0300

    Удалил неиспользуемый import в Comment

--- src/main/java/ru/practicum/shareit/item/comment/Comment.java
+++ src/main/java/ru/practicum/shareit/item/comment/Comment.java
@@ -11,7 +11,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
